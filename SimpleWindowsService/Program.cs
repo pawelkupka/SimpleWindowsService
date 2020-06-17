8da44c0ec0c2eaa872f4bc8f4242a217c104ee6b
pawelkupka <pawel.kupka@gmail.com>
2020-06-17 20:06:05 +0200

    Done

---
+++ SimpleWindowsService/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Topshelf;
+
+namespace SimpleWindowsService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<SimpleService>(s =>
+                {
+                    s.ConstructUsing(heartbeat => new SimpleService());
+                    s.WhenStarted(heartbeat => heartbeat.Start());
+                    s.WhenStopped(heartbeat => heartbeat.Stop());
+                });
+
+                x.RunAsLocalSystem();
+                x.StartManually();
+                x.SetServiceName("SimpleWindowsService");
+                x.SetDisplayName("Simple Windows Service");
+                x.SetDescription("This is the simple windows service.");
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
---
+++ SimpleWindowsService/SimpleService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Timers;
+
+namespace SimpleWindowsService
+{
+    public class SimpleService
+    {
+        private readonly Timer _timer;
+
+        public SimpleService()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] lines = new string[] { DateTime.Now.ToString() };
+            File.AppendAllLines(@"C:\my-services\SimpleWindowsService\data.txt", lines);
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
